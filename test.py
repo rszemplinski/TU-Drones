61e4d65c71f9a6216d1e6f2e24ded33460c282e6
Ryan Szemplinski <rszemplinski22@gmail.com>
2015-10-20 14:01:30 -0400

    We classified the drone tools

--- test.py
+++ test.py
@@ -2,9 +2,17 @@ import os, sys
 sys.path.append(os.getcwd() + "/TU-Drones")
 
 from pymavlink import mavutil
-from tools.drone_tools import *
+from tools.drone_tools import DroneTools
+import time
 
 api = local_connect()
 vehicle = api.get_vehicles()[0]
 
-arm_vehicle(vehicle)
+tools = DroneTools(api, vehicle);
+
+tools.arm_vehicle()
+tools.wait_for_arming()
+time.sleep(2)
+
+print "Landing!"
+tools.disarm_vehicle()
--- tools/drone_tools.py
+++ tools/drone_tools.py
@@ -2,198 +2,204 @@ import time, pdb, log
 import drone_modes as mode
 from droneapi.lib import VehicleMode
 
-def pre_arm_checks(vehicle):
-    print "\nRunning through pre-arm checks..."
-    initialise(vehicle)
-    wait_for_gps(vehicle)
+class DroneTools:
+    def __init__(self, api, vehicle):
+        self.api = api
+        self.vehicle = vehicle
 
-def initialise(vehicle):
-    while vehicle.mode.name == "INITIALISING":
-        print "\nWaiting for the vehicle to initialise"
-        time.sleep(1)
+    def pre_arm_checks(self):
+        print "\nRunning through pre-arm checks..."
+        self.initialise()
+        self.wait_for_gps()
 
-def wait_for_gps(vehicle):
-    while vehicle.gps_0.fix_type < 2:
-        print "\nWaiting for GPS... : ", vehicle.gps_0.fix_type
-        time.sleep(1)
+    def initialise(self):
+        while self.vehicle.mode.name == "INITIALISING":
+            print "\nWaiting for the vehicle to initialise"
+            time.sleep(1)
 
-def arm_and_takeoff(aTargetAltitude):
+    def wait_for_gps(self):
+        while self.vehicle.gps_0.fix_type < 2:
+            print "\nWaiting for GPS... : ", self.vehicle.gps_0.fix_type
+            time.sleep(1)
+
+    def arm_and_takeoff(self, aTargetAltitude):
+        """
+        Arms vehicle and fly to aTargetAltitude.
+        """
+
+        self.pre_arm_checks()
+        self.arm_vehicle()
+        self.wait_for_arming()
+
+        print "Taking off!"
+        self.vehicle.commands.takeoff(aTargetAltitude) # Take off to target altitude
+        self.vehicle.flush()
+
+        # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+        #  after Vehicle.commands.takeoff will execute immediately).
+        while not self.api.exit:
+            print " Altitude: ", self.vehicle.location.alt
+            if self.vehicle.location.alt>=aTargetAltitude*0.95: #Just below target, in case of undershoot.
+                print "Reached target altitude"
+                break;
+            time.sleep(1)
+
+    def prepare_for_takeoff(self):
+        self.vehicle.mode = VehicleMode('STABILIZE')
+        self.vehicle.armed = True
+        self.vehicle.flush()
+        while not self.vehicle.mode.name=='STABILIZE' and not self.vehicle.armed and not self.api.exit:
+            print '\nGetting ready for takeoff!'
+            time.sleep(1)
+
+    def reset(self):
+        print "\nReset vehicle attributes/parameters and exit"
+        self.vehicle.mode = VehicleMode("STABILIZE")
+        self.vehicle.armed = False
+        self.vehicle.parameters['THR_MIN']=130
+        self.vehicle.flush()
+
+    def add_observer(self, type, callback):
+        self.vehicle.add_attribute_observer(type, callback)
+        self.vehicle.flush()
+
+    def remove_observer(self, type, callback):
+        self.vehicle.remove_attribute_observer(type, callback)
+        self.vehicle.flush()
+
+    def wait_for_arming(self):
+        while not self.vehicle.armed and not self.api.exit:
+            print " Waiting for arming..."
+            time.sleep(1)
+
+    def arm_vehicle(self):
+        print "Arming motors"
+        # Copter should arm in STABILIZE mode
+        self.vehicle.mode    = VehicleMode("STABILIZE")
+        self.vehicle.armed   = True
+        self.vehicle.flush()
+
+    def disarm_vehicle(self):
+        print "Disarming Vehicle"
+        self.vehicle.armed = False
+        self.vehicle.flush()
+
+    def set_mode(self, mode):
+        self.vehicle.mode = VehicleMode(mode)
+        self.vehicle.flush()
+
+    def get_parameter(self, name):
+        return self.vehicle.parameters[name]
+
+    def set_parameter(self, name, val):
+        self.vehicle.parameters[name] = val
+        self.vehicle.flush()
+
+    def get_cmds(self):
+        cmds = self.vehicle.commands
+        cmds.download()
+        cmds.wait_valid()
+        return cmds
+
     """
-    Arms vehicle and fly to aTargetAltitude.
+      velocity_x > 0 => fly North
+      velocity_x < 0 => fly South
+      velocity_y > 0 => fly East
+      velocity_y < 0 => fly West
+      velocity_z < 0 => ascend
+      velocity_z > 0 => descend
     """
+    def set_velocity(self, v_x, v_y, v_z):
+        msg = self.vehicle.message_factory.set_position_target_local_ned_encode(
+            0,      #time_boot_ms (not used)
+            0, 0,   #target system, target component
+            mavutil.mavlink.MAV_FRAME_BODY_NED, #frame
+            0b0000111111000111, #type mask (only speeds enabled)
+            0, 0, 0, #x, y, z (not used)
+            v_x, v_y, v_z,
+            0, 0, 0, #x, y, z acceleration (not supported yet)
+            0, 0
+        )
+        self.vehicle.send_mavlink(msg)
+        self.vehicle.flush()
 
-    pre_arm_checks()
-    arm_vehicle()
-    wait_for_arming()
+    def set_yaw(self, heading, relative=False):
+        is_relative = 1 if relative else 0
 
-    print "Taking off!"
-    vehicle.commands.takeoff(aTargetAltitude) # Take off to target altitude
-    vehicle.flush()
-
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.commands.takeoff will execute immediately).
-    while not api.exit:
-        print " Altitude: ", vehicle.location.alt
-        if vehicle.location.alt>=aTargetAltitude*0.95: #Just below target, in case of undershoot.
-            print "Reached target altitude"
-            break;
-        time.sleep(1)
-
-def prepare_for_takeoff():
-    vehicle.mode = VehicleMode('STABILIZE')
-    vehicle.armed = True
-    vehicle.flush()
-    while not vehicle.mode.name=='STABILIZE' and not vehicle.armed and not api.exit:
-        print '\nGetting ready for takeoff!'
-        time.sleep(1)
-
-def reset():
-    print "\nReset vehicle attributes/parameters and exit"
-    vehicle.mode = VehicleMode("STABILIZE")
-    vehicle.armed = False
-    vehicle.parameters['THR_MIN']=130
-    vehicle.flush()
-
-def add_observer(type, callback):
-    vehicle.add_attribute_observer(type, callback)
-    vehicle.flush()
-
-def remove_observer(type, callback):
-    vehicle.remove_attribute_observer(type, callback)
-    vehicle.flush()
-
-def arm_vehicle(vehicle):
-    print "Arming motors"
-    # Copter should arm in STABILIZE mode
-    vehicle.mode    = VehicleMode("STABILIZE")
-    vehicle.armed   = True
-    vehicle.flush()
-
-def wait_for_arming():
-    while not vehicle.armed and not api.exit:
-        print " Waiting for arming..."
-        time.sleep(1)
-
-def disarm_vehicle():
-    vehicle.armed = False
-    vehicle.flush()
-
-def set_mode(mode):
-    vehicle.mode = VehicleMode(mode)
-    vehicle.flush()
-
-def get_parameter(name):
-    return vehicle.parameters[name]
-
-def set_parameter(name, val):
-    vehicle.parameters[name] = val
-    vehicle.flush()
-
-def get_cmds():
-    cmds = vehicle.commands
-    cmds.download()
-    cmds.wait_valid()
-    return cmds
-
-"""
-  velocity_x > 0 => fly North
-  velocity_x < 0 => fly South
-  velocity_y > 0 => fly East
-  velocity_y < 0 => fly West
-  velocity_z < 0 => ascend
-  velocity_z > 0 => descend
-"""
-def set_velocity(v_x, v_y, v_z):
-    msg = vehicle.message_factory.set_position_target_local_ned_encode(
-        0,      #time_boot_ms (not used)
-        0, 0,   #target system, target component
-        mavutil.mavlink.MAV_FRAME_BODY_NED, #frame
-        0b0000111111000111, #type mask (only speeds enabled)
-        0, 0, 0, #x, y, z (not used)
-        v_x, v_y, v_z,
-        0, 0, 0, #x, y, z acceleration (not supported yet)
-        0, 0
-    )
-    vehicle.send_mavlink(msg)
-    vehicle.flush()
-
-def set_yaw(heading, relative=False):
-    is_relative = 1 if relative else 0
-
-    msg = vehicle.message_factory.command_long_encode(
-        0, 0,   #target system, target component
-        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
-        0, #confirmation
-        heading,    #param 1, yaw in degrees
-        0,          #param 2, yaw speed deg/s
-        1,          #param 3, direction -1 ccw, 1 cw
-        is_relative, #param 4, relative offset 1, absolute angle 0
-        0, 0, 0
-    )
-
-    vehicle.send_mavlink(msg)
-    vehicle.flush()
-
-def set_speed(speed):
-    msg = vehicle.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED, #command
-        0, #confirmation
-        0, #param 1
-        speed, # speed in metres/second
-        0, 0, 0, 0, 0 #param 3 - 7
+        msg = self.vehicle.message_factory.command_long_encode(
+            0, 0,   #target system, target component
+            mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+            0, #confirmation
+            heading,    #param 1, yaw in degrees
+            0,          #param 2, yaw speed deg/s
+            1,          #param 3, direction -1 ccw, 1 cw
+            is_relative, #param 4, relative offset 1, absolute angle 0
+            0, 0, 0
         )
 
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-    vehicle.flush()
+        self.vehicle.send_mavlink(msg)
+        self.vehicle.flush()
 
-"""
-  ROI == Region of Interest
-  Have no clue what this is for
-"""
-def set_roi(location):
-    # create the MAV_CMD_DO_SET_ROI command
-    msg = vehicle.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_DO_SET_ROI, #command
-        0, #confirmation
-        0, 0, 0, 0, #params 1-4
-        location.lat,
-        location.lon,
-        location.alt
-        )
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-    vehicle.flush()
+    def set_speed(self, speed):
+        msg = self.vehicle.message_factory.command_long_encode(
+            0, 0,    # target system, target component
+            mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED, #command
+            0, #confirmation
+            0, #param 1
+            speed, # speed in metres/second
+            0, 0, 0, 0, 0 #param 3 - 7
+            )
 
-def set_home(aLocation, aCurrent=1):
-    msg = vehicle.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_DO_SET_HOME, #command
-        0, #confirmation
-        aCurrent, #param 1: 1 to use current position, 2 to use the entered values.
-            0, 0, 0, #params 2-4
-        aLocation.lat,
-        aLocation.lon,
-        aLocation.alt
-        )
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-    vehicle.flush()
+        # send command to vehicle
+        self.vehicle.send_mavlink(msg)
+        self.vehicle.flush()
 
-def get_vehicle_status(vehicle):
-    print "\nGet all vehicle attribute values:"
-    print "\nLocation: %s" % vehicle.location
-    print "\nAttitude: %s" % vehicle.attitude
-    print "\nVelocity: %s" % vehicle.velocity
-    print "\nGPS: %s" % vehicle.gps_0
-    print "\nGroundspeed: %s" % vehicle.groundspeed
-    print "\nAirspeed: %s" % vehicle.airspeed
-    print "\nMount status: %s" % vehicle.mount_status
-    print "\nBattery: %s" % vehicle.battery
-    print "\nRangefinder: %s" % vehicle.rangefinder
-    print "\nRangefinder distance: %s" % vehicle.rangefinder.distance
-    print "\nRangefinder voltage: %s" % vehicle.rangefinder.voltage
-    print "\nMode: %s" % vehicle.mode.name    # settable
-    print "\nArmed: %s" % vehicle.armed    # settable
+    """
+      ROI == Region of Interest
+      Have no clue what this is for
+    """
+    def set_roi(self, location):
+        # create the MAV_CMD_DO_SET_ROI command
+        msg = self.vehicle.message_factory.command_long_encode(
+            0, 0,    # target system, target component
+            mavutil.mavlink.MAV_CMD_DO_SET_ROI, #command
+            0, #confirmation
+            0, 0, 0, 0, #params 1-4
+            location.lat,
+            location.lon,
+            location.alt
+            )
+        # send command to vehicle
+        self.vehicle.send_mavlink(msg)
+        self.vehicle.flush()
+
+    def set_home(self, aLocation, aCurrent=1):
+        msg = self.vehicle.message_factory.command_long_encode(
+            0, 0,    # target system, target component
+            mavutil.mavlink.MAV_CMD_DO_SET_HOME, #command
+            0, #confirmation
+            aCurrent, #param 1: 1 to use current position, 2 to use the entered values.
+                0, 0, 0, #params 2-4
+            aLocation.lat,
+            aLocation.lon,
+            aLocation.alt
+            )
+        # send command to vehicle
+        self.vehicle.send_mavlink(msg)
+        self.vehicle.flush()
+
+    def get_vehicle_status(self):
+        print "\nGet all vehicle attribute values:"
+        print "\nLocation: %s" % self.vehicle.location
+        print "\nAttitude: %s" % self.vehicle.attitude
+        print "\nVelocity: %s" % self.vehicle.velocity
+        print "\nGPS: %s" % self.vehicle.gps_0
+        print "\nGroundspeed: %s" % self.vehicle.groundspeed
+        print "\nAirspeed: %s" % self.vehicle.airspeed
+        print "\nMount status: %s" % self.vehicle.mount_status
+        print "\nBattery: %s" % self.vehicle.battery
+        print "\nRangefinder: %s" % self.vehicle.rangefinder
+        print "\nRangefinder distance: %s" % self.vehicle.rangefinder.distance
+        print "\nRangefinder voltage: %s" % self.vehicle.rangefinder.voltage
+        print "\nMode: %s" % self.vehicle.mode.name    # settable
+        print "\nArmed: %s" % self.vehicle.armed    # settable
