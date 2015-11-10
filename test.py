3a911ead6e4479881c87c9299a9619488695f6de
bsell <bryantsell@gmail.com>
2015-11-10 13:58:56 -0500

    Upgrade dronekit in requirements txt

--- test.py
+++ test.py
@@ -2,11 +2,13 @@ from dronekit import connect
 from tools.drone_tools import DroneTools
 import tools.log as log
 import tools.drone_modes as mode
-import ipdb, time
+import ipdb, time, dronekit
 
 log.stop_log()
 
-vehicle = connect('/dev/ttyUSB2', await_params = True)
+print "Starting connection..."
+
+vehicle = connect('/dev/ttyUSB0', baud=57600)
 
 tools = DroneTools(vehicle);
 
